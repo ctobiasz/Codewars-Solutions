f8032f4a55b74f524c37b02a4811ab57ccce20da
Christopher Tobiasz <44296783+ctobiasz@users.noreply.github.com>
2019-10-14 20:47:10 -0400

    Update buildASquare.js

--- buildASquare.js
+++ buildASquare.js
@@ -4,12 +4,12 @@
 //Array(5) === [ undefined, undefined, undefined, undefined, undefined ]
 
 //b.fill("+".repeat(5)) === Fill() replaces all the undefined values with the string "+" and repeat() takes the entire 5 strings
-                            and considers all 5 values like 1 string and it repeats the entire string 5 times.
+//                          and considers all 5 values like 1 string and it repeats the entire string 5 times.
 //Array(5) [ "+++++", "+++++", "+++++", "+++++", "+++++" ]
 
 
 //b.join("\n") === Lastly, join will take the 5 new strings of pluses within the array and concat them into one new string with
-                   the rule to ('\n') which means to skip a line after every string.
+//                 the rule to ('\n') which means to skip a line after every string.
 //"+++++
 // +++++
 // +++++
